c304f6130f59e32c0fa5a7de1194af75cb7c8165
semenov <semenov@skeeks.com>
2019-04-08 12:33:23 +0300

    dev

---
+++ src/config/common.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+return [
+    'components' => [
+        'urlManager' => [
+            'class' => \skeeks\yii2\multiLanguage\MultiLangUrlManager::class,
+        ]
+    ]
+];
---
+++ src/config/web.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+return [
+    'components' => [
+        'multiLanguage' => [
+            'class' => \skeeks\yii2\multiLanguage\MultiLangComponent::class
+        ],
+
+        'request' => [
+            'class'               => \skeeks\yii2\multiLanguage\MultiLangRequest::class
+        ]
+    ]
+];
